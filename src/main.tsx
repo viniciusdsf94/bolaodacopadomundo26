6f22b394885d6f1960c896798a189a4b5ae48128
Vinícius <viniciusdsf94@gmail.com>
2026-06-28 10:29:51 -0300

    feat: force SW check and auto reload on update

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Forçar atualização do PWA assim que houver uma nova versão disponível
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.update().catch((err) => {
+      console.warn("Erro ao buscar atualização do SW:", err);
+    });
+  });
+
+  let refreshing = false;
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    if (!refreshing) {
+      refreshing = true;
+      window.location.reload();
+    }
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
